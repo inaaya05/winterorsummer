441a31b386dfd9a35de3c809c21f4d3f4588c09c
inaaya05 <128428669+inaaya05@users.noreply.github.com>
2023-12-12 20:06:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ var questionCount = 0;
 
 var result = document.getElementById("result");
 
-/*var q1a1 = document.getElementById("q1a1");
+var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
 
 var q2a1 = document.getElementById("q2a1");
 var q2a2 = document.getElementById("q2a2");
 
 var q3a1 = document.getElementById("q3a1");
-var q3a2 = document.getElementById("q3a2");*/
+var q3a2 = document.getElementById("q3a2");
 
 //var restart = document.getElementById("restart");
 
